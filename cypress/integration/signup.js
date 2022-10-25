f4008f25ba88f1f56ae4dca423cdc6024b8a71c7
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-10-25 16:07:40 -0300

    Agrupando os cenários com Describe

--- cypress/integration/signup.js
+++ cypress/integration/signup.js
@@ -1,76 +1,81 @@
-it('deve cadastrar um novo usuario',()=>{
- 
-   
-
-  const dados = {
-     name : 'Thiago Vasconcelos',
-     email: 'thiago@samuraibs.com',
-     password: 'pwd123'
+describe('cadastro', () => {
 
 
-  }
-   cy.task('removeUser',dados.email)
-   .then((result)=>{
-    console.log(result)
-       
+   it('deve cadastrar um novo usuario', () => {
+
+
+
+      const dados = {
+         name: 'Thiago Vasconcelos',
+         email: 'thiago@samuraibs.com',
+         password: 'pwd123'
+
+
+      }
+      cy.task('removeUser', dados.email)
+         .then((result) => {
+            console.log(result)
+
+         })
+
+      cy.visit('/signup')
+
+      cy.get('input[placeholder="Nome"]').type(dados.name)
+      cy.get('input[placeholder="E-mail"]').type(dados.email)
+      cy.get('input[placeholder="Senha"]').type(dados.password)
+
+
+      cy.contains('button', 'Cadastrar').click()
+
+
+
+      cy.get('.toast')
+         .should('be.visible')
+         .find('p')
+         .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!')
+
+
+
+
+
    })
 
-    cy.visit('/signup')
 
-    cy.get('input[placeholder="Nome"]').type(dados.name)
-    cy.get('input[placeholder="E-mail"]').type(dados.email)
-    cy.get('input[placeholder="Senha"]').type(dados.password)
 
-    
-    cy.contains('button','Cadastrar').click()
-
-   
-
-    cy.get('.toast')
-    .should('be.visible')
-    .find('p')
-    .should('have.text','Agora você pode fazer seu login no Samurai Barbershop!') 
+   it('deve exibir e-mail ja cadastrado', () => {
 
 
 
-    
+      const dados = {
+         name: 'Thiago Vasconcelos',
+         email: 'thiago@samuraibs.com',
+         password: 'pwd123'
+
+
+      }
+
+
+      cy.visit('/signup')
+
+      cy.get('input[placeholder="Nome"]').type(dados.name)
+      cy.get('input[placeholder="E-mail"]').type(dados.email)
+      cy.get('input[placeholder="Senha"]').type(dados.password)
+
+
+
+
+      cy.contains('button', 'Cadastrar').click()
+
+
+      cy.get('.toast')
+         .should('be.visible')
+         .find('p')
+         .should('have.text', 'Email já cadastrado para outro usuário.')
+
+
+
+
+
+   })
 
 })
-
-
-
-it('deve exibir e-mail ja cadastrado',()=>{
- 
-   
-
-   const dados = {
-      name : 'Thiago Vasconcelos',
-      email: 'thiago@samuraibs.com',
-      password: 'pwd123'
- 
- 
-   }
-    
- 
-     cy.visit('/signup')
- 
-     cy.get('input[placeholder="Nome"]').type(dados.name)
-     cy.get('input[placeholder="E-mail"]').type(dados.email)
-     cy.get('input[placeholder="Senha"]').type(dados.password)
- 
-     
- 
- 
-     cy.contains('button','Cadastrar').click()
- 
- 
-     cy.get('.toast')
-     .should('be.visible')
-     .find('p')
-     .should('have.text','Email já cadastrado para outro usuário.') 
- 
- 
- 
-     
- 
- })
