5aef34fd94c8820deb49c4e6b96193af20206a79
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-10-25 15:11:40 -0300

    Submetendo e validando o cadastro (Bônus: Faker.js)

--- cypress/integration/signup.js
+++ cypress/integration/signup.js
@@ -1,8 +1,12 @@
+import { faker } from '@faker-js/faker'
+
 it('deve cadastrar um novo usuario',()=>{
+ 
+   
 
   const dados = {
      name : 'Thiago Vasconcelos',
-     email: 'thiago@samuraibs.com',
+     email: faker.internet.email(),
      password: 'pwd123'
 
 
@@ -14,5 +18,16 @@ it('deve cadastrar um novo usuario',()=>{
     cy.get('input[placeholder="Nome"]').type(dados.name)
     cy.get('input[placeholder="E-mail"]').type(dados.email)
     cy.get('input[placeholder="Senha"]').type(dados.password)
+
+    cy.contains('button','Cadastrar').click()
+
+    cy.get('.toast')
+    .should('be.visible')
+    .find('p')
+    .should('have.text','Agora você pode fazer seu login no Samurai Barbershop!') 
+
+
+
+    
 
 })
