69097872cb28f002aa9d8f7abf9566b5089f9d82
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-10-25 16:05:38 -0300

    Cobertura de testes com dependência de cenários

--- cypress/integration/signup.js
+++ cypress/integration/signup.js
@@ -21,16 +21,10 @@ it('deve cadastrar um novo usuario',()=>{
     cy.get('input[placeholder="E-mail"]').type(dados.email)
     cy.get('input[placeholder="Senha"]').type(dados.password)
 
-    // cy.intercept('POST','/users',{
-
-    //   statusCode: 200
-       
-    // }).as('postUser')
-
-
+    
     cy.contains('button','Cadastrar').click()
 
-   // cy.wait('@postUser')
+   
 
     cy.get('.toast')
     .should('be.visible')
@@ -42,3 +36,41 @@ it('deve cadastrar um novo usuario',()=>{
     
 
 })
+
+
+
+it('deve exibir e-mail ja cadastrado',()=>{
+ 
+   
+
+   const dados = {
+      name : 'Thiago Vasconcelos',
+      email: 'thiago@samuraibs.com',
+      password: 'pwd123'
+ 
+ 
+   }
+    
+ 
+     cy.visit('/signup')
+ 
+     cy.get('input[placeholder="Nome"]').type(dados.name)
+     cy.get('input[placeholder="E-mail"]').type(dados.email)
+     cy.get('input[placeholder="Senha"]').type(dados.password)
+ 
+     
+ 
+ 
+     cy.contains('button','Cadastrar').click()
+ 
+ 
+     cy.get('.toast')
+     .should('be.visible')
+     .find('p')
+     .should('have.text','Email já cadastrado para outro usuário.') 
+ 
+ 
+ 
+     
+ 
+ })
